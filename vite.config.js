d465732baa28eeff4e4e5d9226643dba1d3a502a
vmwebio <vmwebio@gmail.com>
2024-04-24 18:51:56 +0300

    add optimize plugin for assets

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,48 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import cleanPlugin from 'vite-plugin-clean';
 
 export default defineConfig({
     root: './src',
     publicDir: '../public',
     build: {
         outDir: '../dist',
-    }
+    },
+    plugins: [
+        [
+            cleanPlugin()
+        ],
+        ViteImageOptimizer({
+            svg: {
+                multipass: true,
+                plugins: [
+                  {
+                    name: 'preset-default',
+                    params: {
+                      overrides: {
+                        cleanupNumericValues: false,
+                        removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                      },
+                      cleanupIDs: {
+                        minify: false,
+                        remove: false,
+                      },
+                      convertPathData: false,
+                    },
+                  },
+                  'sortAttrs',
+                  {
+                    name: 'addAttributesToSVGElement',
+                    params: {
+                      attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                    },
+                  },
+                ],
+              },
+            webp: {
+                // https://sharp.pixelplumbing.com/api-output#webp
+                lossless: true,
+              },
+        }),
+      ],
 })
